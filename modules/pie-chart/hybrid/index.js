9e89e4eaa71f254dce4d6fb3e9b26d8092b44b15
Evan Gatchell <greygatch@gmail.com>
2017-02-09 10:37:29 -0600

    Place log for quick identification.

--- modules/pie-chart/hybrid/index.js
+++ modules/pie-chart/hybrid/index.js
@@ -19,6 +19,7 @@ import merge from 'lodash.merge';
 
 
 const color = schemeCategory20;
+console.log('LOG: How to construct pie chart?' );
 // pie().value((d) => d.value).sort(null)
 
 const getSliceFill = (d, i) => (
